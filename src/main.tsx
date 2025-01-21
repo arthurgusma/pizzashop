cf150367374165453cddd496869d201b5bd1d0cc
gusmadev <arthur.cabj@hotmail.com>
2025-01-21 13:02:16 -0300

    feat: usage of react-router and react-helmet-async

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router";
 import { router } from './routes.tsx';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <HelmetProvider>
+    <Helmet titleTemplate='%s | puzza.shop' />
+    <RouterProvider router={router} />
+  </HelmetProvider>
 )
