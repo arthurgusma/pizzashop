3e45921d89deb611a3d70b74f8377582cf7c0dd8
gusmadev <arthur.cabj@hotmail.com>
2025-01-22 15:57:29 -0300

    feat: orders

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -3,10 +3,8 @@ import { Header } from '@/components/header'
 
 export function AppLayout() {
     return (
-        <div>
-            <div className="flex min-h-screen flex-col antialiased">
-                <Header />
-            </div>
+        <div className="flex min-h-screen flex-col antialiased">
+            <Header />
             <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
                 <Outlet />
             </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,13 @@ import { SignIn } from "./pages/auth/signin";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/signup";
+import { Orders } from "./pages/app/orders/orders";
 
 
 export const router = createBrowserRouter([
     { path: '/', element: <AppLayout />, children: [
-        { path: '/', element: <Dashboard /> }
+        { path: '/', element: <Dashboard /> },
+        { path: '/orders', element: <Orders /> }
     ]},
     { path: '/', element: <AuthLayout />, children: [
         { path: '/sign-in', element: <SignIn /> },
