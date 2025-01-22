f3cf0c83778dbfbc2dd2ae5ea02fa83b59447faa
gusmadev <arthur.cabj@hotmail.com>
2025-01-22 15:08:20 -0300

    feat: app header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from 'sonner';
+import { ThemeProvider } from "./components/theme-provider";
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate='%s | puzza.shop' />
-      <Toaster richColors />
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+      <HelmetProvider>
+        <Helmet titleTemplate='%s | puzza.shop' />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
