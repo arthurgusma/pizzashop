ffd3c43c0384634dc1b0b9b5cebf14560c76016e
gusmadev <arthur.cabj@hotmail.com>
2025-01-27 17:25:34 -0300

    feat: 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,10 +5,13 @@ import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/signup";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 
 export const router = createBrowserRouter([
-    { path: '/', element: <AppLayout />, children: [
+    { path: '/', element: <AppLayout />, 
+        errorElement: <NotFound />,
+        children: [
         { path: '/', element: <Dashboard /> },
         { path: '/orders', element: <Orders /> }
     ]},
